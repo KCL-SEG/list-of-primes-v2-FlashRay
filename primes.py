68cd12873aeeeb17e65656b0eea12f32335d08dd
FlashRay <114407988+FlashRay@users.noreply.github.com>
2022-10-02 19:57:25 +0100

    Update primes.py

--- primes.py
+++ primes.py
@@ -1,6 +1,11 @@
-"""List of prime numbers generator."""
-"""ENTER YOUR SOLUTION HERE!"""
+import math
 
 def primes(number_of_primes):
     list = []
+    for x in range (2, number_of_primes):
+        for y in range (2,int(math.sqrt(x)+1)):
+            if x%y==0 :
+                break
+        else:
+            list.append(x)
     return list
