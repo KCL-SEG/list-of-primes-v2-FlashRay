bef28e63b1ff6e1f77185f4ee874ee25f10e492b
Sultan <ali.sultan@kcl.ac.uk>
2022-10-14 00:45:03 +0100

    Finish Task

--- primes.py
+++ primes.py
@@ -1,11 +1,20 @@
-import math
+def isPrime(num):
+
+    for x in range (2,int(num**0.5)+1):
+        if num%x==0 :
+            return False
+    
+    return True
+
 
 def primes(number_of_primes):
     list = []
-    for x in range (2, number_of_primes):
-        for y in range (2,int(math.sqrt(x)+1)):
-            if x%y==0 :
-                break
-        else:
-            list.append(x)
+    if number_of_primes < 1:
+        raise ValueError("Positive integers only")
+    i=2
+    while (len(list) < number_of_primes):
+        if isPrime(i):
+            list.append(i)
+        i+=1
+
     return list
